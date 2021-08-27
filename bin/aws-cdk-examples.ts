004c9e6696fc91771df17897ee8791dd36068c90
James Collerton <james.collerton@bbc.co.uk>
2021-08-27 13:27:35 +0100

    Added stack for creating code bucket

--- bin/aws-cdk-examples.ts
+++ bin/aws-cdk-examples.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { AwsCdkExamplesStack } from '../lib/aws-cdk-examples-stack';
+import { CodeS3BucketStack } from '../lib/code-s3-bucket-stack';
 
 const app = new cdk.App();
-new AwsCdkExamplesStack(app, 'AwsCdkExamplesStack', {
+new CodeS3BucketStack(app, 'CodeS3BucketStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- test/aws-cdk-examples.test.ts
+++ test/aws-cdk-examples.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as AwsCdkExamples from '../lib/aws-cdk-examples-stack';
+import * as AwsCdkExamples from '../lib/code-s3-bucket-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new AwsCdkExamples.AwsCdkExamplesStack(app, 'MyTestStack');
+    const stack = new AwsCdkExamples.CodeS3BucketStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
